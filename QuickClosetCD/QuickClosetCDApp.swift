511e892c16de8b90c50a3baf6279eff34e192b00
Katherine Palevich <katherine.palevich@gmail.com>
2020-10-20 10:24:06 -0700

    Initial Commit

---
+++ QuickClosetCD/QuickClosetCDApp.swift
@@ -0,0 +1,20 @@
+//
+//  QuickClosetCDApp.swift
+//  QuickClosetCD
+//
+//  Created by Katherine Palevich on 10/20/20.
+//
+
+import SwiftUI
+
+@main
+struct QuickClosetCDApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
