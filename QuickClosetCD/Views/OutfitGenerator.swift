2e0876646bf1d5225e56923fe8739fef75732d6e
Katherine Palevich <katherine.palevich@gmail.com>
2021-01-14 21:13:47 -0800

    Continue to Implement OpenWeatherAPI

--- QuickClosetCD/Views/OutfitGenerator.swift
+++ QuickClosetCD/Views/OutfitGenerator.swift
@@ -7,10 +7,95 @@
 
 import SwiftUI
 
+let icon : [String: String] = [
+    "01d" : "sun.max",
+    "01n" : "sun.max.fill",
+    "02d" : "cloud.sun",
+    "02n" : "cloud.sun.fill",
+    "03d" : "cloud",
+    "03n" : "cloud.moon.fill",
+    "04d" : "cloud",
+    "04n" : "cloud.moon.fill",
+    "09d" : "cloud.heavyrain",
+    "09n" : "cloud.heavyrain.fill",
+    "10d" : "cloud.drizzle",
+    "10n" : "cloud.drizzle.fill",
+    "11d" : "cloud.bolt.rain",
+    "11n" : "cloud.bolt.rain.fill",
+    "13d" : "cloud.snow",
+    "13n" : "cloud.snow.fill",
+    "50d" : "cloud.fog",
+    "50n" : "cloud.fog.fill"
+]
+
 struct OutfitGenerator: View {
     @State private var current: Current?
     var body: some View {
-        Text("\(current?.name ?? "")").onAppear(perform: loadData)
+        VStack {
+            HStack{
+                Text("\(current?.name ?? "")")
+                    Text(Date().addingTimeInterval(600), style: .date)
+            }.onAppear(perform: loadData)
+            Image(systemName: icon[current?.weather[0].icon ?? ""] ?? "").onAppear(perform: loadData)
+            Text("\(current?.weather[0].main ?? "")").onAppear(perform: loadData)
+            HStack{
+                Text("Temperature: \(temp)").onAppear(perform: loadData)
+                Text("Feels Like: \(feelsLike)").onAppear(perform: loadData)
+            }
+            Text("Min/Max:  \(tempMin) / \(tempMax) ")
+        }
+    }
+    
+    private var temp : String {
+        if let temp = current?.main.temp {
+            let formatter = MeasurementFormatter()
+            formatter.locale = Locale.current
+            formatter.numberFormatter.maximumFractionDigits = 0
+            let measurement = Measurement(value: temp, unit: UnitTemperature.kelvin)
+            return formatter.string(from: measurement)
+        }
+        else {
+            return "N/A"
+        }
+    }
+    
+    private var feelsLike : String {
+        if let feelsLike = current?.main.feels_like {
+            let formatter = MeasurementFormatter()
+            formatter.locale = Locale.current
+            formatter.numberFormatter.maximumFractionDigits = 0
+            let measurement = Measurement(value: feelsLike, unit: UnitTemperature.kelvin)
+            return formatter.string(from: measurement)
+        }
+        else {
+            return "N/A"
+        }
+    }
+    
+    private var tempMin : String {
+        if let tempMin = current?.main.temp_min {
+            let formatter = MeasurementFormatter()
+            formatter.locale = Locale.current
+            formatter.numberFormatter.maximumFractionDigits = 0
+            let measurement = Measurement(value: tempMin, unit: UnitTemperature.kelvin)
+            return formatter.string(from: measurement)
+        }
+        else {
+            return "N/A"
+        }
+    }
+    
+    private var tempMax : String {
+        if let tempMax = current?.main.temp_max {
+            let formatter = MeasurementFormatter()
+            formatter.locale = Locale.current
+            formatter.numberFormatter.maximumFractionDigits = 0
+            let measurement = Measurement(value: tempMax, unit: UnitTemperature.kelvin)
+            return formatter.string(from: measurement)
+        }
+        else {
+            return "N/A"
+        }
     }
     
     // Not my own code, got it from this link: https://www.hackingwithswift.com/books/ios-swiftui/sending-and-receiving-codable-data-with-urlsession-and-swiftui
